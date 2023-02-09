88dfa15d02ffe5268e13acfb3c80646289efb48d
Abhilash <abhilash.rpee18@gmail.com>
2023-02-09 10:39:47 +0530

    api added

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { Container , Stack } from '@mui/system'
 import { Button, TextField, Typography } from '@mui/material'
 import {makeStyles} from '@material-ui/styles'
@@ -35,35 +35,65 @@ const useStyles = makeStyles({
 });
 
 const Login = () => {
+    /*Hooks starts here */
+const [email , setEmail] = useState("");
+const [password , setPassword] = useState("");
 const classes = useStyles();
 
 
+/* button function starts here*/
 const handleSubmit = (e) =>{
+    
+}
+const handleCancel = (e) =>{
     e.preventDefault();
 }
-const handleCancel = () =>{
-    alert("clicked");
-}
+const URL = "http://demo.emeetify.com:8080/daytodaytask/admin/adminlogin";
+console.log(URL);
+fetch(URL,{
+    method:"POST",
+    body:JSON.stringify({
+        email:email,
+        password: password
+    }) ,
+    header:{
+        'Content-type':'application/json; charset=UTF-8'
+    }
+})
+.then((response)=>{response.json()}).then((data)=>{console.log(data)}).catch((err)=>{console.log(err.message ,"eeeee")});
+
+
+
+
+/*button function ends here */
     return(
         <>
         <Container component='form'  maxWidth='sm' className={classes.background}>
             <Typography className={classes.heading} sx={{marginTop:'50px'}} variant='h4' >Login Page</Typography>
+
             <Stack className={classes.usernameTextfield} >
-                <TextField label='UserName' variant='filled' />
+                <TextField label='UserName' 
+                variant='filled' 
+                value={email}
+                onChange={(e)=> {setEmail(e.target.value)}}
+               
+                />
             </Stack>
+
             <Stack className={classes.passwordTextfield}>
-                <TextField label='Password' variant='filled' />
+                <TextField value={password} onChange={(e)=> {setPassword(e.target.value)}} label='Password' variant='filled' />
             </Stack>
+
             <Stack direction='row'>
-            <Stack className={classes.submitBtn} direction='row'>
+                <Stack className={classes.submitBtn} direction='row'>
                 <Button  type='submit' onClick={handleSubmit}  className={classes.submitBtn} variant='contained'>Submit</Button>
-            </Stack>
-            <Stack className={classes.cancelBtn}>
-                <Button   className={classes.cancelBtn} variant='contained'>Cancel</Button>
-            </Stack>
-            </Stack>
-           
+                </Stack>
+                <Stack className={classes.cancelBtn}>
+                <Button onClick={handleCancel}   className={classes.cancelBtn} variant='contained'>Cancel</Button>
+                </Stack>
+            </Stack> 
         </Container> 
+        
         </>
        
     )
